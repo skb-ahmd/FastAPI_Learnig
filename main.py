9c257cbf294408c17752b30dee1333e3e62d5859
skb-ahmd <ahmedshakeeb69@gmail.com>
2023-09-13 00:48:45 +0530

    Get a speific data from the list with id

--- main.py
+++ main.py
@@ -19,19 +19,31 @@ my_posts = [{"title": "Title of post 1", "content": "Content of post 1", "id": 1
     "title": "Title of post 1", "content": "Content of post 1", "id": 2}]
 
 
+def find_post(id):
+    for post in my_posts:
+        if post["id"]==id:
+            return post
+
+
 @app.get("/")
 def root():
     return {"Message": "Hello Qorld"}
 
 
 @app.get("/posts")
-def read_item():
+def get_posts():
     return {"data": my_posts}
 
 
 @app.post("/posts")
 def create_post(post: Post):
-    post_dict=post.dict()
-    post_dict['id']=randrange(0,9999999)
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 9999999)
     my_posts.append(post_dict)
     return {"New Post": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id : int):
+    post=find_post(id)
+    return {"post_details": post}
