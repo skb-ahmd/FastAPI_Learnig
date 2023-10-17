87b3a6592c8ef1565cd66688dbb0874dea7c46fe
skbahmd <ahmedshakeeb69@gmail.com>
2023-10-17 17:22:49 +0530

    Add post with ORM concept

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from random import randrange
 from typing import Optional, Union
 
-from fastapi import FastAPI, Response, status, HTTPException,Depends
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 import psycopg2
@@ -9,13 +9,12 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 from . import models
-from . database import engine,get_db
+from . database import engine, get_db
 # test 1
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 
 class Post(BaseModel):
@@ -59,24 +58,29 @@ def find_index_post(id):
 def root():
     return {"Message": "Hello Qorld"}
 
+
 @app.get("/sqlalchemy")
 def test(db: Session = Depends(get_db)):
-    posts=db.query(models.Post).all()
-    return{"data":posts}
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    posts=db.query(models.Post).all()
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute(""" Insert into posts (title,content,published) Values (%s,%s,%s) Returning * """,
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(""" Insert into posts (title,content,published) Values (%s,%s,%s) Returning * """,
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"New Post": new_post}
 
 
@@ -106,9 +110,8 @@ def delete_post(id: int):
 def update_post(id: int, post: Post):
     cursor.execute(""" Update posts set title = %s ,content = %s,published = %s where id = %s  Returning * """,
                    (post.title, post.content, post.published, id))
-    updated_post=cursor.fetchone()
+    updated_post = cursor.fetchone()
     conn.commit()
-
 
     if updated_post == None:
         raise HTTPException(
